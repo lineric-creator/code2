5a09e393897eaba3c64c63816168170705c0a0ec
lineric-creator <lin.eric@iisgalvanimi.edu.it>
2025-11-14 09:50:17 +0000

    2

--- script.js
+++ script.js
@@ -12,32 +12,17 @@ function buttonFunction() {
 function buttonFunction1() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
- 
-  ctx.fillStyle = "#00FF00";
-    ctx.fillRect(0, 0, 10, 10);
-ctx.fillRect(20, 20, 10, 10);
-  ctx.fillRect(40, 40, 10, 10);
-  ctx.fillRect(60, 60, 10, 10);
-   ctx.fillRect(80, 80, 10, 10);
-   ctx.fillRect(90, 90, 10, 10);
-  ctx.fillRect(100, 100, 10, 10);
-  
-   ctx.fillRect(120, 120, 10, 10);
-   ctx.fillRect(140, 140, 10, 10);
-  ctx.fillRect(160, 160, 10, 10);
-   ctx.fillRect(180, 180, 10, 10);
-  ctx.fillRect(200, 200, 10, 10);
-  ctx.fillRect(180, 0, 10, 10);
-  ctx.fillRect(160, 20, 10, 10);
-  ctx.fillRect(140, 40, 10, 10);
-  ctx.fillRect(120, 60, 10, 10);
-   ctx.fillRect(100, 80, 10, 10);
-  ctx.fillRect(80, 100, 10, 10);
-   ctx.fillRect(60, 120, 10, 10)
-   ctx.fillRect(40, 140, 10, 10);
-  ctx.fillRect(20, 160, 10, 10);
-   ctx.fillRect(0, 180, 10, 10);
 
+  ctx.fillStyle = "#00eeffff";
+
+  
+  for (let i = 0; i <= 10; i++) {
+    ctx.fillRect(i * 20, i * 20, 10, 10);
+  }
+
+  for (let i = 0; i <= 9; i++) {
+    ctx.fillRect(180 - i * 20, i * 20, 10, 10);
+  }
 }
 
 
